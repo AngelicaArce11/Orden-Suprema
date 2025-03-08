e29132ac2386c7e826db4566dc5745f091e1b604
yeehll02 <116982357+yeehll02@users.noreply.github.com>
2025-03-08 02:48:59 -0500

    Parte 1 confirmar mision

--- OrdenSuprema/src/components/Assassin/DebtsConfirm.tsx
+++ OrdenSuprema/src/components/Assassin/DebtsConfirm.tsx
@@ -2,17 +2,65 @@ import React, { useState, useEffect } from "react";
 import { NavBar } from "../../elements/NavBar";
 import { Dropdown, DropdownItem } from "flowbite-react";
 import { TableElement } from "../../elements/Table2";
+import axios from 'axios';
 
 
-const assassins = [
-    {deudor: 'Jack el Destripador', acreedor: 'Zodiaco', descripcion: 'Deuda 1', estado: 'Incompleta', comprobante:''},
-    {deudor: 'Zodiaco', acreedor: 'Jack el Destripador', descripcion: 'Deuda 2', estado: 'Incompleta', comprobante:''},
-    {deudor: 'Darth Vader', acreedor: 'Jack el Destripador', descripcion: 'Deuda 3', estado: 'Incompleta', comprobante:''},
-    {deduor: 'Chanci care picha', acreedor: 'Jack el Destripador', descripcion: 'Deuda 4', estado: 'Incompleta', comprobante:''},
-    {deudor: 'Jack el Destripador', acreedor: 'Zodiaco', descripcion: 'Deuda 5', estado: 'Incompleta', comprobante:'dddd'},
-]
+// const assassins = [
+//     {deudor: 'Jack el Destripador', acreedor: 'Zodiaco', descripcion: 'Deuda 1', estado: 'Incompleta', comprobante:''},
+//     {deudor: 'Zodiaco', acreedor: 'Jack el Destripador', descripcion: 'Deuda 2', estado: 'Incompleta', comprobante:''},
+//     {deudor: 'Darth Vader', acreedor: 'Jack el Destripador', descripcion: 'Deuda 3', estado: 'Incompleta', comprobante:''},
+//     {deduor: 'Chanci care picha', acreedor: 'Jack el Destripador', descripcion: 'Deuda 4', estado: 'Incompleta', comprobante:''},
+//     {deudor: 'Jack el Destripador', acreedor: 'Zodiaco', descripcion: 'Deuda 5', estado: 'Incompleta', comprobante:'dddd'},
+// ]
 
 export const DebtsConfirm = () => {
+        const [debts, setDebts] = useState<Debt[]>([])
+        const [names, setNames] = useState<Record<number, string>>({})
+
+        useEffect(() => {
+            axios.get("http://localhost:3000/debt")
+              .then(({ data }) => setDebts(data))
+              .catch(console.error);
+          }, []);
+
+        const namePerson = async (id: number) => {
+            if (!names[id]) {  
+              const { data } = await axios.get(`http://localhost:3000/UserById/${id}`);
+              setNames(prev => ({ ...prev, [id]: data.name }));    
+            }
+          };
+      
+          // Obtener los nombres de los usuarios
+        useEffect(() => {
+              debts.forEach(({ creditorId, debtorId }) => {
+                  namePerson(creditorId);
+                  namePerson(debtorId);
+              });
+          }, [debts]);
+
+        const debtsData = debts
+            .filter(m => !m.is_completed)
+            .map(({ debtorId, creditorId, description }) => ({
+                deudor: names[debtorId], 
+                acreedor: names[creditorId],  
+                descripcion: description,
+                estado: "Incompleta",
+              }));    
+
+        // useEffect(() => {
+        //     axios.get(`http://localhost:3000/debts`)
+        //         .then(({ data }) => setDebts(data))
+        //         .catch(console.error);
+        // }, []);
+
+        // const handleAccept = async (id: number) => {
+        //     await axios.put(`http://localhost:3000/debts/${id}`);
+        //     setDebts(debts.map(debt => 
+        //         debt.id === id ? { ...debt, estado: "Completa" } : debt
+        //     ));
+        // };
+
+
         const buttons = [
             {name: 'Aceptar', color: 'greenToBlue', onClick: () => console.log('Aceptar')},
             {name: 'Rechazar', color: 'pinkToOrange', onClick: () => console.log('Rechazar')}
@@ -28,7 +76,7 @@ export const DebtsConfirm = () => {
 
             {/* Tabla con los datos filtrados */}
             <div className='w-full pt-15 pr-4 pl-4 px-2 sm:px-30'> 
-                <TableElement header={['Deudor', 'Acreedor', 'Descripción', 'Estado']} showModalColumn={true} data={assassins} buttons = {buttons} />
+                <TableElement header={['Deudor', 'Acreedor', 'Descripción', 'Estado']} showModalColumn={true} data={debtsData} buttons = {buttons} />
             </div>
         </>
     );
