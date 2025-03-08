6f33fbdddb5fc77a6f263829ee7ef484a00466f2
Capry2512 <frquinteroc@unal.edu.co>
2025-03-07 21:28:37 -0500

    comm

--- OrdenSuprema/backend/controllers/debtController.js
+++ OrdenSuprema/backend/controllers/debtController.js
@@ -9,6 +9,38 @@ export const getAllDebts = async (req, res) => {
     }
 };
 
+//Controlador para obtener todas las deudas por usuario
+export const getDebtByCreditorId = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const creditorId = Number(id);
+
+        if (!creditorId || creditorId <= 0) {
+            return res.status(400).json({ message: 'ID inválido' });
+        }
+
+        const creditorExists = await Creditor.findByPk(creditorId);
+        if (!creditorExists) {
+            return res.status(404).json({ message: 'Acreedor no encontrado' });
+        }
+
+        const debtCreditor = await Debt.findAll({
+            where: {
+                creditorId
+            }
+        });
+
+        if (debtCreditor.length === 0) {
+            return res.status(404).json({ message: 'Deudas no encontradas' });
+        }
+
+        res.json(debtCreditor);
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
 export const createDebt = async (req, res) => {
     try {
         const {description, creditorId, debtorId} = req.body
--- OrdenSuprema/backend/database/sequelize.js
+++ OrdenSuprema/backend/database/sequelize.js
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize'
 export const sequelize = new Sequelize(
     'OrdenSuprema',
     'postgres',
-    'postgres123',
+    'capry2512',
     {
         host: 'localhost',
         dialect: 'postgres'
