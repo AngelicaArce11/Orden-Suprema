7f2f17e3eab41bf21a861b55c23d0f77fd76f44c
EuronG <juandigiraldoj@gmail.com>
2025-03-08 02:45:15 -0500

    ups, DB

--- OrdenSuprema/backend/database/sequelize.js
+++ OrdenSuprema/backend/database/sequelize.js
@@ -3,14 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    'OrdenSuprema',
-    'postgres',
-    'postgres123',
-    {
-        host: 'localhost',
-        dialect: 'postgres'
-    });
+export const sequelize = new Sequelize(process.env.DATABASE_URL,{
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, 
+        }
+    },
+    logging: false 
+});
 
     // 'OrdenSuprema',
     // 'postgres',
@@ -19,4 +21,4 @@ export const sequelize = new Sequelize(
     //     host: 'localhost',
     //     dialect: 'postgres'
     // }
-// )
+// );
